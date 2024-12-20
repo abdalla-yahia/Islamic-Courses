cf07101ad1c0f70375c4696c73e6a16033c11d0e
ابو يحيى عبدالله <abdalla-yahia>
2024-12-20 19:27:30 +0200

    Edite Upload File

--- src/app/api/v1/upload/route.tsx
+++ src/app/api/v1/upload/route.tsx
@@ -17,18 +17,17 @@ const UPLOAD_VEDIOS_DIR = path.resolve(ROOT_PATH ?? "", "public/uploads/videos")
 export const POST = async (req: NextRequest) => {
   const formData = await req.formData();
   const body = Object.fromEntries(formData);
-  const file = (body.file as unknown as  {
-    arrayBuffer(): WithImplicitCoercion<ArrayBuffer | SharedArrayBuffer> | PromiseLike<WithImplicitCoercion<ArrayBuffer | SharedArrayBuffer>>;name:''
-}) || null;
+  const file = (body.file) || null;
 
   if (file) {
+    console.log('first file',file)
     if (!fs.existsSync(UPLOAD_DIR)) {
         fs.mkdirSync(UPLOAD_DIR);
     }
       const imageExtensions = ['.jpg','.jpeg','.png','.gif','.bmp','.tiff','.webp','.svg','.heif','.raw'];
-      const Extention = file?.name?.slice(file?.name?.indexOf('.'), )
+      const Extention = (file as File)?.name?.slice((file as File)?.name?.indexOf('.'), )
           if(imageExtensions.includes(Extention)){
-              const buffer = Buffer.from(await file.arrayBuffer());
+              const buffer = Buffer.from(await (file as File).arrayBuffer());
               if (!fs.existsSync(UPLOAD_IMAGES_DIR)) {
                 fs.mkdirSync(UPLOAD_IMAGES_DIR);
             }
@@ -38,7 +37,7 @@ export const POST = async (req: NextRequest) => {
               );
           }
           if(Extention === '.pdf'){
-            const buffer = Buffer.from(await file.arrayBuffer());
+            const buffer = Buffer.from(await (file as File).arrayBuffer());
               if (!fs.existsSync(UPLOAD_FILES_DIR)) {
                 fs.mkdirSync(UPLOAD_FILES_DIR);
             }
@@ -48,7 +47,7 @@ export const POST = async (req: NextRequest) => {
               );
           }
           if(Extention === '.mp3' || Extention === '.wav' || Extention === '.ogg' || Extention === '.flac'){
-            const buffer = Buffer.from(await file.arrayBuffer());
+            const buffer = Buffer.from(await (file as File).arrayBuffer());
               if (!fs.existsSync(UPLOAD_AUDIOS_DIR)) {
                 fs.mkdirSync(UPLOAD_AUDIOS_DIR);
             }
@@ -58,7 +57,7 @@ export const POST = async (req: NextRequest) => {
               );
           }
           if(Extention === '.mp4' || Extention === '.avi' || Extention === '.mov' || Extention === '.flv'){
-            const buffer = Buffer.from(await file.arrayBuffer());
+            const buffer = Buffer.from(await (file as File).arrayBuffer());
               if (!fs.existsSync(UPLOAD_VEDIOS_DIR)) {
                 fs.mkdirSync(UPLOAD_VEDIOS_DIR);
             }
