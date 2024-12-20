a3cb71e0335b21232e7fe65ddb59a3f8ced4c50c
ابو يحيى عبدالله <abdalla-yahia>
2024-12-20 03:37:46 +0200

    Delete Upload Folder

--- src/app/api/v1/upload/route.tsx
+++ src/app/api/v1/upload/route.tsx
@@ -1,18 +1,18 @@
 import { NextRequest, NextResponse } from "next/server";
 import path from "path";
 import fs from "fs";
-// import { ROOT_PATH } from '@/Utils/Constant'
+import { ROOT_PATH } from '@/Utils/Constant'
 
-// const UPLOAD_DIR = path.resolve(ROOT_PATH ?? "", "public/uploads");
-// const UPLOAD_IMAGES_DIR = path.resolve(ROOT_PATH ?? "", "public/uploads/images");
-// const UPLOAD_FILES_DIR = path.resolve(ROOT_PATH ?? "", "public/uploads/files");
-// const UPLOAD_AUDIOS_DIR = path.resolve(ROOT_PATH ?? "", "public/uploads/audios");
-// const UPLOAD_VEDIOS_DIR = path.resolve(ROOT_PATH ?? "", "public/uploads/videos");
-const UPLOAD_DIR = path.join(process.cwd(), "public", "uploads");
-const UPLOAD_IMAGES_DIR = path.join(process.cwd(), "public/uploads", "images");
-const UPLOAD_FILES_DIR = path.join(process.cwd(), "public/uploads", "files");
-const UPLOAD_AUDIOS_DIR = path.join(process.cwd(), "public/uploads", "audios");
-const UPLOAD_VEDIOS_DIR = path.join(process.cwd(), "public/uploads", "videos");
+const UPLOAD_DIR = path.resolve(ROOT_PATH ?? "", "public/uploads");
+const UPLOAD_IMAGES_DIR = path.resolve(ROOT_PATH ?? "", "public/uploads/images");
+const UPLOAD_FILES_DIR = path.resolve(ROOT_PATH ?? "", "public/uploads/files");
+const UPLOAD_AUDIOS_DIR = path.resolve(ROOT_PATH ?? "", "public/uploads/audios");
+const UPLOAD_VEDIOS_DIR = path.resolve(ROOT_PATH ?? "", "public/uploads/videos");
+// const UPLOAD_DIR = path.resolve("public/uploads");
+// const UPLOAD_IMAGES_DIR = path.resolve("public/uploads/images");
+// const UPLOAD_FILES_DIR = path.resolve("public/uploads/files");
+// const UPLOAD_AUDIOS_DIR = path.resolve("public/uploads/audios");
+// const UPLOAD_VEDIOS_DIR = path.resolve("public/uploads/videos");
 
 export const POST = async (req: NextRequest) => {
   const formData = await req.formData();
@@ -23,14 +23,14 @@ export const POST = async (req: NextRequest) => {
 
   if (file) {
     if (!fs.existsSync(UPLOAD_DIR)) {
-        fs.mkdirSync(UPLOAD_DIR, { recursive: true });
+        fs.mkdirSync(UPLOAD_DIR);
     }
       const imageExtensions = ['.jpg','.jpeg','.png','.gif','.bmp','.tiff','.webp','.svg','.heif','.raw'];
       const Extention = file?.name?.slice(file?.name?.indexOf('.'), )
           if(imageExtensions.includes(Extention)){
               const buffer = Buffer.from(await file.arrayBuffer());
               if (!fs.existsSync(UPLOAD_IMAGES_DIR)) {
-                fs.mkdirSync(UPLOAD_IMAGES_DIR, { recursive: true });
+                fs.mkdirSync(UPLOAD_IMAGES_DIR);
             }
             fs.writeFileSync(
                 path.resolve(UPLOAD_IMAGES_DIR, (body.file as File).name),
@@ -40,7 +40,7 @@ export const POST = async (req: NextRequest) => {
           if(Extention === '.pdf'){
             const buffer = Buffer.from(await file.arrayBuffer());
               if (!fs.existsSync(UPLOAD_FILES_DIR)) {
-                fs.mkdirSync(UPLOAD_FILES_DIR, { recursive: true });
+                fs.mkdirSync(UPLOAD_FILES_DIR);
             }
             fs.writeFileSync(
                 path.resolve(UPLOAD_FILES_DIR, (body.file as File).name),
@@ -50,7 +50,7 @@ export const POST = async (req: NextRequest) => {
           if(Extention === '.mp3' || Extention === '.wav' || Extention === '.ogg' || Extention === '.flac'){
             const buffer = Buffer.from(await file.arrayBuffer());
               if (!fs.existsSync(UPLOAD_AUDIOS_DIR)) {
-                fs.mkdirSync(UPLOAD_AUDIOS_DIR, { recursive: true });
+                fs.mkdirSync(UPLOAD_AUDIOS_DIR);
             }
             fs.writeFileSync(
                 path.resolve(UPLOAD_AUDIOS_DIR, (body.file as File).name),
@@ -60,7 +60,7 @@ export const POST = async (req: NextRequest) => {
           if(Extention === '.mp4' || Extention === '.avi' || Extention === '.mov' || Extention === '.flv'){
             const buffer = Buffer.from(await file.arrayBuffer());
               if (!fs.existsSync(UPLOAD_VEDIOS_DIR)) {
-                fs.mkdirSync(UPLOAD_VEDIOS_DIR, { recursive: true });
+                fs.mkdirSync(UPLOAD_VEDIOS_DIR);
             }
             fs.writeFileSync(
                 path.resolve(UPLOAD_VEDIOS_DIR, (body.file as File).name),
