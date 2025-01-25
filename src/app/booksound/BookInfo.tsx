c9b7d5a27a05941c6ecabd7b09efe68502eeb10e
ابو يحيى عبدالله <abdalla-yahia>
2025-01-25 02:47:17 +0200

    Edit Player Audio And Finish Add Akhsar Almokhtasarat

--- src/app/booksound/BookInfo.tsx
+++ src/app/booksound/BookInfo.tsx
@@ -3,9 +3,12 @@ import Image from "next/image";
 import Link from "next/link";
 import * as icon from '@/Components/Icons/icons'
 export default function BookInfo({Book}:{Book:Chapter}) {
+
   return (
     <div className="flex justify-start items-start gap-3 wi-full mt-2">
+        {/* Book Image */}
         <Image src={Book?.image} width={60} height={60} alt={Book?.title} />
+        {/* Book Info */}
         <div className="w-full h-full gap-3 flex flex-col pt-2">
             <div className="flex justify-start text-gray-800 items-center w-full gap-3 flex-nowrap mb-2">
                 <h2>اسم الكتاب :
@@ -25,9 +28,9 @@ export default function BookInfo({Book}:{Book:Chapter}) {
                 <h2>تاريخ النشر :
                     <span className="text-red-500">{Book?.year}</span>
                 </h2>
-                {Book?.download &&<h2>تحميل الكتاب PDF :
-                    <span className="text-red-500">{
-                        <Link href={Book?.download} target="_blank" download={true} className="cursor-pointer text-blue-500 flex">
+                {Book?.download &&<h2 className="flex flex-nowrap">تحميل الكتاب PDF :
+                    <span className="text-red-500 flex">{
+                        <Link href={Book?.download} target="_blank" download={true} className="cursor-pointer text-blue-500 hover:text-blue-900 flex">
                         اضغط هنا للتحميل <icon.FaDownload />
                         </Link>
                     }</span>
