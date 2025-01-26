a8080b8f52258afe6b1b1826cce2c095181e492b
ابو يحيى عبدالله <abdalla-yahia>
2025-01-26 15:27:45 +0200

    Add A New Book Manhag Elsalkeen

--- src/app/booksound/Data/index.ts
+++ src/app/booksound/Data/index.ts
@@ -1,7 +1,8 @@
 import Sera from './Sera.json'
 import Feqh_Hanbaly from './Feqh_Hanbaly.json'
 import Asol_Feqh from './Asol_Feqh.json'
-
+import Aqeda from './Aqeda.json'
+import Qadaya_Manhagea from './Qadaya_Manhajea.json'
 export default function BooksData(){
-    return [Sera,Feqh_Hanbaly,Asol_Feqh]
+    return [Sera,Feqh_Hanbaly,Asol_Feqh,Qadaya_Manhagea,Aqeda]
 }
