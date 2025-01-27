eef5b22e67df215020705a17a01cd2bdd5f73e79
ابو يحيى عبدالله <abdalla-yahia>
2025-01-27 20:50:40 +0200

    Edit Structure of Data Books

--- src/Components/Books/Data/FeqhHanbaly/index.ts
+++ src/Components/Books/Data/FeqhHanbaly/index.ts
@@ -3,7 +3,9 @@ import Akhsar_Mokhtasarat from './Akhsar_Mokhtasarat.json';
 import Manar_Elsabil from './Manar_Elsabil.json';
 import Elrawd_ElMorbeaa from './Elrawd_Elmorbea.json';
 import Zad_ElMostaqneaa from './Zad_Elmostaqneaa.json';
-import Mokhtasr_ElKheraky from './Mokhtasar_Elkheraky.json'
+import Mokhtasr_ElKheraky from './Mokhtasar_Elkheraky.json';
+import Elrahabia from './AlRahbia.json';
+
 export default function Feqh_Hanbalyan(){
     return {
         id:2,
@@ -12,6 +14,7 @@ export default function Feqh_Hanbalyan(){
         books:[
             Manhaj_Salkeen,
             Akhsar_Mokhtasarat,
+            Elrahabia,
             Manar_Elsabil,
             Elrawd_ElMorbeaa,
             Zad_ElMostaqneaa,
