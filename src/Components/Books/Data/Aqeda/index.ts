f975505a8d26fde926e1ff80b68daeb8399d9864
ابو يحيى عبدالله <abdalla-yahia>
2025-01-29 15:24:44 +0200

    Add Tafseer Bn Katheer

--- src/Components/Books/Data/Aqeda/index.ts
+++ src/Components/Books/Data/Aqeda/index.ts
@@ -1,4 +1,5 @@
 import salasat_Osol from './Salasat_Osol.json';
+import KaashF_Al_Shobhat from './Kashf_AL_Shobhat.json';
 import Lomat_Eatekad from './Lomat_Al_Eatekad.json'
 import Kawaed_Mothla from './Kaqwaed_Mothla.json';
 import Kawaed_Mothla_Mansour from './Kaqwaed_Mothla_Mansour.json';
@@ -10,7 +11,8 @@ import MenaTwo from './AlMena2.json';
 import Fath_ElMajeed from './Fath_ElMajeed.json';
 import Fath_ElMajeed_Mansour from './Fath_ElMajeed_Mansour.json';
 import ElKawaed_ElKolya_Mansour from './ElKaqwaed_Alkolya_Mansour.json'
-import Masalet_Eman_Mansour from './Masalet_Aleman_Mansour.json'
+import Masalet_Eman_Mansour from './Masalet_Aleman_Mansour.json';
+import Mareej_Al_Kabool from './Marej_Al_Kabool.json';
 import MarejOne from './Marej_Part_One.json';
 import MarejTwo from './Marej_Part_Two.json';
 import MarejThree from './Marej_Part_Three.json';
@@ -23,6 +25,7 @@ export default function Aqeda_Books(){
         description:"عقيدة",
         books:[
             salasat_Osol,
+            KaashF_Al_Shobhat,
             Lomat_Eatekad,
             Kawaed_Mothla,
             Kawaed_Mothla_Mansour,
@@ -35,6 +38,7 @@ export default function Aqeda_Books(){
             Fath_ElMajeed_Mansour,
             ElKawaed_ElKolya_Mansour,
             Masalet_Eman_Mansour,
+            Mareej_Al_Kabool,
             MarejOne,
             MarejTwo,
             MarejThree,
--- src/Components/Books/Data/Hadith/index.ts
+++ src/Components/Books/Data/Hadith/index.ts
@@ -1,12 +1,21 @@
+import Arbaeen_Nawawya from './Arbaeen_Nawawya.json';
+import Ryad_El_Salhin from './Ryad_Al_Salheen.json';
 import Saheh_Muslem from './Sahih_Muslem.json';
-
+import Saheh_El_Bokhary from './Sahih_El_Bokhary.json';
+import Sonan_AL_Nasaee from './Sonn_Al_Nasaee.json';
+import Sonan_AL_Termezy from './Sonn_Al_Termezy.json';
 export default function Hadith_Books(){
     return {
         id:8,
         title:"شروح الحديث ",
         description:"شروح الحديث ",
         books:[
-            Saheh_Muslem
+            Arbaeen_Nawawya,
+            Ryad_El_Salhin,
+            Sonan_AL_Nasaee,
+            Sonan_AL_Termezy,
+            Saheh_Muslem,
+            Saheh_El_Bokhary,
         ]
     }
 }
