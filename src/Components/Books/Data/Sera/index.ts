114dee15f118c0a71af64b193845e1dce28e7fe0
ابو يحيى عبدالله <abdalla-yahia>
2025-01-31 01:35:53 +0200

    add alraheek

--- src/Components/Books/Data/Sera/index.ts
+++ src/Components/Books/Data/Sera/index.ts
@@ -2,6 +2,7 @@ import Sor_Mn_Hayat_ElSahaba from './Sor_Sahaba.json';
 import Sor_Mn_Hayat_Tabeean from './Sor_Tabeean.json';
 import Mokhtasar_Sera from './Mokhtasr_Sera.json';
 import Ashra_Mobashroon from './Ashra_Mobashroon.json';
+import Al_Raheek_Al_Makhtoom from './Rahek_Makhtoom.json';
 export default function Sera_Books(){
     return {
         id:1,
@@ -11,7 +12,8 @@ export default function Sera_Books(){
             Sor_Mn_Hayat_ElSahaba,
             Sor_Mn_Hayat_Tabeean,
             Mokhtasar_Sera,
-            Ashra_Mobashroon
+            Ashra_Mobashroon,
+            Al_Raheek_Al_Makhtoom
         ]
     }
 }
