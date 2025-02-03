55926cff103eab103292428c6ead62731dc69cc6
ابو يحيى عبدالله <abdalla-yahia>
2025-02-03 16:23:38 +0200

    Add Some New Books and sections

--- src/Components/Books/Data/Kadaya_Manhajea/index.ts
+++ src/Components/Books/Data/Kadaya_Manhajea/index.ts
@@ -3,6 +3,7 @@ import Walaa_We_Baeaa_Mansour from './Walaa_Baraa_Mansour.json';
 import Kada_Kadar_Mansour from './Kadaa_Kadar_Mansour.json';
 import ElHakmya_Mansour from './Elhakmea_Mansour.json';
 import ElAmal_AlGamaee_Mansour from './Elamal_ElGamaee_Mansour.json';
+import Malameh_Salaf from './Malameh_Rayesea.json';
 import Derasat_Fe_Elnasranya from './Derasat_Nasranya.json';
 import Manahej_Istdla_Sona from './Manahej_Istdlal_Sona_Mansour.json'
 export default function Kadaya_Books(){
@@ -11,20 +12,12 @@ export default function Kadaya_Books(){
         title:"قضايا فكرية ",
         description:"قضايا فكرية ",
         books:[
-            // Khelaf,
-            // Eman_We_Kofr,
             Eman_We_Kofr_Mansour,
             Walaa_We_Baeaa_Mansour,
             Kada_Kadar_Mansour,
             ElHakmya_Mansour,
             ElAmal_AlGamaee_Mansour,
-            // Masalaeh_We_Mafased,
-            // Walaa_We_Baeaa,
-            // Kada_We_Kadar,
-            // Elamal_Gamaee,
-            // Amr_Maroof,
-            // Elhakmea,
-            // ElJehad,
+            Malameh_Salaf,
             Derasat_Fe_Elnasranya,
             Manahej_Istdla_Sona
         ]
--- src/Components/Books/Data/Tazkea/index.ts
+++ src/Components/Books/Data/Tazkea/index.ts
@@ -1,5 +1,8 @@
 import Al_Daa_We_Al_Dawaa from './Al_Daa_We_Al_Dawaa.json';
 import Rafe_Al_Malam from './Rafe_Al_Malam.json';
+import Moshkelat_1 from './Moshkelat_1.json';
+import Moshkelat_2 from './Moshkelat_2.json';
+import Moshkelat_3 from './Moshkelat_3.json';
 export default function Tarbea_Books(){
     return {
         id:12,
@@ -7,7 +10,10 @@ export default function Tarbea_Books(){
         describetion:'تربية و تزكية',
         books:[
             Al_Daa_We_Al_Dawaa,
-            Rafe_Al_Malam
+            Rafe_Al_Malam,
+            Moshkelat_1,
+            Moshkelat_2,
+            Moshkelat_3
         ]
     }
 }
