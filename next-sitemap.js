ec16731c12f43c5528b81d0e504065584e4d36a9
ابو يحيى عبدالله <abdalla-yahia>
2025-07-04 23:02:36 +0300

    Add Generate Sitemap

---
+++ next-sitemap.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://khaled-mansour.vercel.app', // غيرها للدومين بتاعك لو اشتريت دومين
+  generateRobotsTxt: true, // علشان يولد ملف robots.txt كمان
+  sitemapSize: 7000, // لو عندك عدد كبير من الصفحات
+};
